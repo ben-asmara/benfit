c88441e61ab78632737c8e73cc546ee80c5cbafe
Ben Daouda Issa <139893646+ben-asmara@users.noreply.github.com>
2026-09-12 22:17:48 -0700

    Delete sw.js

--- sw.js
+++
@@ -1,6 +0,0 @@
-const CACHE="benfit-v2";
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["/","/manifest.webmanifest"]))));
-self.addEventListener("fetch",e=>{
-  if(e.request.method!=="GET") return;
-  e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)));
-});
